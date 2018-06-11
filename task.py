891d0c68d7f6d4f70b0ff975f894b5ffbcafdf93
Zorro30 <gaurang@Gaurangs-MacBook-Air.local>
2018-06-11 21:26:30 +0530

    Updated Code

--- task.py
+++ task.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 all = input ("Enter all the task you have separated with a ','!\n")
 task= all.split(",")
@@ -7,4 +8,12 @@ print ("This are the task in your tasks list:")
 print (task)
 
 x=random.choice(task)
-print ("This the task '%s' selected on Random! I hope you will complete and won't disappoint me!" %(x))
+print ("The task '%s' is selected on Random! I hope you will complete and won't disappoint me!" %(x))
+
+time.sleep(2)
+
+print("I hope the task is in progress!!!")
+#Since the task is in progress I would remove the task and print the left ones.
+task.remove(x)
+
+print("The remaining list of task is: %s"%(task))
